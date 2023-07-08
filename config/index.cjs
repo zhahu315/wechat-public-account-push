41cc942ac278fe650447c183a5a6905d665f7cf4
Zhang Chi <1120286713@qq.com>
2023-07-08 18:59:06 +0800

    test6.0

--- config/index.cjs
+++ config/index.cjs
@@ -96,7 +96,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oqCLc6QurH3v_va0SV8yp53A0I8g',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'UVXeFkq7NF-dFDt1B_odyC5lQ-lKekUGqnO02pWgq-E',
+      useTemplateId: 'VGtJGXmyRdW0l1Ry6TwNEb4Evyw746y_a6O1dIheGi8',
       province: '江苏',
       // 所在城市或县区
       city: '无锡',
